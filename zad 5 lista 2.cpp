e896d14caebbd9fbb2454bea54059aa88e17780e
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:59:10 +0100

    Add files via upload

---
+++ zad 5 lista 2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int zmiana(char tekst[], int ROZMIAR)
+{
+
+    int x = strlen(tekst);
+    int OOOO = 0;
+    for (int i = 0; i < x + 1; i++)
+    {
+      if (tekst[i] == 'o' || tekst[i] == 'O')
+            if (tekst[i + 1] == 'o' || tekst[i + 1] == 'O')
+            {
+                OOOO++;
+            }
+      if (tekst[i] == 0)
+      {
+            break;
+      }
+    }
+
+
+    return OOOO;
+}
+
+int main()
+{
+    const int ROZMIAR = 1024;
+    char tekst[ROZMIAR];
+    cout << "wprowadź tekst: ";
+    cin.getline(tekst, 1024);
+    int OOOO = zmiana(tekst, ROZMIAR);
+    cout << "\nliczba powtorzen (oo) to: " << OOOO;
+
+}
