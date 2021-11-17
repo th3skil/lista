0229e73107f60e1c7f03ca066743a85de4c3c949
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:03:49 +0100

    Add files via upload

---
+++ zad 11 lista 2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double liczby[10];
+	for (int i = 0;i < 10;i++)
+	{
+		cout << " podaj liczbe z przecinkiem lub bez jak wolisz!: ";
+		cin >> liczby[i];
+
+	}
+
+
+
+	long double max = 0;
+	for (int i = 0;i < 10;i++)
+	{
+
+		if (liczby[i] > max)
+		{
+			max = liczby[i];
+		}
+	}
+	cout << "najwieksza liczba jest: " << max;
+
+	long double min = 100000000000000;
+		for (int i = 0;i < 10; i++)
+		{
+			if (liczby[i] < min)
+			{
+				min = liczby[i];
+			}
+			
+		}
+		cout << "najwieksza liczba jest: " << max;
+		cout << "\nnajmniejsza liczba jest: " << min;
+		
+
+		return 0;
+}
+
