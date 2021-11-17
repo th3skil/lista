abb7cfadbd005d12e73f69beb31740f2573fc1c4
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:53:18 +0100

    Add files via upload

---
+++ zad 4 lista 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b;
+    cout <<"podaj 1 liczbe: ";
+    cin >> a;
+    cout << "podaj 2 liczbe:  ";
+    cin >> b;
+    cout << endl;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+    double c=(a+b)/2;
+    cout << "srednia z" << a << " i " << b << " to " << c << endl;
+   
+    system("pause");
+
+
+}
+
