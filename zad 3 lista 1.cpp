36aa38baaf8050f13444ee2956df7ca4422ef5b5
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:52:42 +0100

    Add files via upload

---
+++ zad 3 lista 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "╔"
+        << "══════"
+        << "╗" << endl;
+
+    cout << "║ żółw ║" << endl;
+
+    cout << "╚"
+        << "══════"
+        << "╝" << endl;
+
+    return 0;
+}
