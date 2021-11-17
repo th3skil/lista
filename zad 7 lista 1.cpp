5965f694d9cc3168ce43f40b7268fdf22ca885a2
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:01:45 +0100

    Add files via upload

---
+++ zad 7 lista 1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x, i, j, k;
+    cout << "Wpisz wielkosc choinki: ";
+    cin >> x;
+    for (i = 1;i <= x - 1;i++)
+    {
+        for (j = x - 2;j >= i;j--)
+        {
+            cout << " ";
+
+        }
+        for (k = 1;k <= (2 * i - 1);k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+       
+    }
+    int ile{};
+    do
+    {
+        cout << " ";
+        ile++;
+    } while (ile <= (x - 3));
+    cout << "#" << endl;
+  int c{};
+  for (c = 1;c <= (2 * i - 3);c++)
+  {
+   cout << "-";
+  }
+        cout << endl;
+    return 0;
+}
