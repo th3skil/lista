de46baaa0dc69dadd244cfb6e51cc12e887329e7
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:59:54 +0100

    Add files via upload

---
+++ zad 6 lista 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double a, b, c, x1, x2, x;
+	cout << "podaj wspolrzedna a:";
+	cin >> a;
+	cout<< "podaj wspolrzedna b:";
+	cin >> b;
+	cout<< "podaj wspolrzedna c:";
+	cin >> c;
+	cout << "*************************************" << endl;
+	cout << "podaj x0: ";
+	cin >> x;
+	cout << "*************************************" << endl;
+	cout << "f(" << x << ") =" << a << "x^2 " << "+" << b << "x" << "+" << c << endl;
+
+	cout << "*************************************" << endl;
+	cout << "f(" << x << ")" << "=" << (a * x * x) + (b * x) + c << endl;
+
+
+
+
+
+
+	system("pause");
+
+
+
+}
