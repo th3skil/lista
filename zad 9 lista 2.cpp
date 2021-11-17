1f71bfcdaa14619993646f8613f34af9c65565fd
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:02:18 +0100

    Add files via upload

---
+++ zad 9 lista 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int* pierwsza_tablica[20];
+	cout << "rozmiar tablicy typu int[20]: " << sizeof pierwsza_tablica;
+	const float* druga_tablica[20];
+	cout << "\nrozmiar tablicy typu float[20]: " << sizeof druga_tablica;
+	const double* trzecia_tablica[20];
+	cout << "\nrozmiar tablicy typu double[20]: " << sizeof trzecia_tablica;
+	const int* czwarta_tablica[5][2];
+	cout << "\nrozmiar tablicy typu int[5][4]: " << sizeof czwarta_tablica;
+	cout << "\n***************************************************************";
+
+
+
+	return 0;
+
+}
