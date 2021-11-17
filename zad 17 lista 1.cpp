b61b2d28148eaad459ac4bf78c154569a2f7c357
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:10:01 +0100

    Add files via upload

---
+++ zad 17 lista 1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int potega(int podstawa, int wykladnik = 2);
+double potega(double podstawa, int wykladnik = 2);
+void potega(int& wynik, int podstawa, int wykladnik);
+int main()
+{
+	double podstawa, wykladnik = 0;
+	cout << "podaj podstawe potegi: ";
+	cin >> podstawa;
+	cout << " podaj wykladnik: ";
+	cin >> wykladnik;
+	cout << "\n ************************************************* \n";
+
+	cout << "potega typu int: " << potega(podstawa, wykladnik) << endl;
+	cout <<"potega typu double:  " << potega(podstawa, wykladnik) << endl;
+
+	int wynik;
+	potega(wynik, podstawa, wykladnik);
+	cout << "wynik  pobrany przez wskaznik: " << wynik << endl;
+		return 0;
+}
+int potega(int podstawa, int wykladnik)
+{
+	return pow(podstawa, wykladnik);
+}
+void potega(int& wynik, int podstawa, int wykladnik) {
+	wynik = pow(podstawa, wykladnik);
+}
+double potega(double podstawa, int wykladnik) {
+	return pow(podstawa, wykladnik);
+}
