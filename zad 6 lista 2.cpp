6ee263a73e4a803e4fd31cd0c1242982cf274875
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:01:26 +0100

    Add files via upload

---
+++ zad 6 lista 2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void wypiszBinarnie(int n) 
+{
+    const int ROZMIAR = 32;
+    bool bity[ROZMIAR]{};
+
+    int licznik = 1;
+    while (n != 0) {
+        bool bit = n % 2;
+        n = n / 2;
+        bity[ROZMIAR - licznik] = bit;
+        licznik++;
+    }
+
+    for (int i = 0; i < ROZMIAR; i++) {
+        if (i % 8 == 0 && i != 0) {
+            cout << " ";
+        }
+
+        cout << bity[i];
+    }
+
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "podaj liczbę: ";
+    cin >> n;
+    wypiszBinarnie(n);
+}
