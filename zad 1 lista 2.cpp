7a707c081d112b2a9c1e768978a06a8a51ac0658
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:50:40 +0100

    Add files via upload

---
+++ zad 1 lista 2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int zliczLiteryA(char tekst[], int ROZMIAR) {
+    int liczbaLiterA = 0;
+    for (int i = 0; i < ROZMIAR; i++) {
+        if (tekst[i] == 0) {
+            break;
+        }
+
+        if (tekst[i] == 'a' || tekst[i] == 'A') {
+            liczbaLiterA++;
+        }
+    }
+
+    return liczbaLiterA;
+}
+
+
+
+
+int main() {
+    const int ROZMIAR = 1024;
+    char tekst[ROZMIAR];
+    cout << "wprowadź tekst: ";
+    cin.getline(tekst,1024);
+
+    int liczbaLiterA = zliczLiteryA(tekst, ROZMIAR);
+    cout << "liczba wystąpień litery \"a\" lub \"A\": " << liczbaLiterA << endl;
+    return 0;
+}
