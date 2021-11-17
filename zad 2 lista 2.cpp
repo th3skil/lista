24d8db66be8b35d58c0c87e94237503f2513e406
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:51:43 +0100

    Add files via upload

---
+++ zad 2 lista 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void odwroc(char tekst[], int rozmiar) {
+    int słowo;
+
+    for (int i = 0; i <= rozmiar; i++) {
+        if (tekst[i] == 0) {
+                słowo = i;
+            break;
+        }
+    }
+
+    for (int i = słowo - 1; tekst[i] != 0; i--) {
+        cout << tekst[i];
+    }
+
+    cout << endl;
+}
+
+int main() {
+    const int ROZMIAR = 1024;
+    char tekst[ROZMIAR];
+    cout << "wprowadź tekst: ";
+    cin.getline(tekst, ROZMIAR);
+    odwroc(tekst, ROZMIAR);
+}
