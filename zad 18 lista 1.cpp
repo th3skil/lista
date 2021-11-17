5a1e1f5d639514c665ecbacfe6716fb8125d8750
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:10:16 +0100

    Add files via upload

---
+++ zad 18 lista 1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	cout << "1 zadanie : \n" << "I believe that the saying that today you cannot be someone without your own \nidentifying brand is true that every person should work on their brand,\n that represents us\n";
+	cout << "2 zadanie : \n" << "a personal brand allows us to show ourselves and thus make us noticeable This helps \ n many people to keep or gain a job\n";
+	cout << "3 zadanie : \n" << "on my own t-shirt I would put my name, which would be swept up by the flash out function, \nthis function is that it reflects the light when taking a photos with the use of a lamp\n";
+	
+		system("pause");
+}
