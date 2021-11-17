71ab70ba137b12e3e7f49740db407fefa2156c6d
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:09:41 +0100

    Add files via upload

---
+++ zad 16 2 proba.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+bool czyPierwsza(int n)
+{
+
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+}
+int main()
+{
+    int liczba;
+    cout << "podaj liczbe";
+    cin >> liczba;
+    bool wynik = czyPierwsza(liczba);
+    cout << " liczba pierwsza?  " << boolalpha << wynik << endl;
+
+
+
+    cout << "liczby pierwsze < 100:" << endl;
+    for (int j = 0; j <= 10;j++)
+    {
+        for (int i = 0; i < 100; i++) {
+            if (czyPierwsza(i)) {
+                cout << i << "  ";
+            }
+
+
+        }
+        if (j % 10 == 0) {
+            cout << endl;
+        }
+
+        system("pause");
+    }
