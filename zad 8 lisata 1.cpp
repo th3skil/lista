161ce9518e7f591acd668a6f7ae801334c872f35
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:02:36 +0100

    Add files via upload

---
+++ zad 8 lisata 1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int liczba, wielokrotnosc;
+ cout << "podaj liczbe:  ";
+ cin >> liczba;
+ cout << "podaj wielokrotnosc:  ";
+ cin >> wielokrotnosc;
+
+ for (int i = 1;i <= liczba;i++)
+ {
+	 for (int j = 1;j <= wielokrotnosc;j++)
+	 {
+		 if (j * i < 10 )
+		 {
+			 cout << j * i << "  ";
+		 }
+		 else {
+			 cout << i * j << " ";
+			}
+	 }
+	 cout << endl;
+ }
+
+ system("pause");
+}
+
