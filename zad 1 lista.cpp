9555f6a030877aa883df3c26bf73de42d4d5d100
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:50:09 +0100

    Add files via upload

---
+++ zad 1 lista.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int ilosc;
+	cout << "podaj ilosc gwiazdek :";
+	cin >> ilosc;
+	for (int i = 1;i <= ilosc;i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	system("pause");
+
+}
+
