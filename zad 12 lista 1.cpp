af06471480ed71d9c3ccd5650f0e2956c1f00a4c
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 08:04:04 +0100

    Add files via upload

---
+++ zad 12 lista 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int liczba = 43, a = 8, b = 10;
+
+	cout.width(30);
+
+	cout << "Szerokość tego pola to 30" << endl;
+
+	//wyrównanie liczb do prawej strony 
+	//(na całą liczbę przeznaczamy siedem miejsc)
+	//puste miejsca będą wypełnione spacjami
+
+	cout.width(7);
+	cout << liczba << endl;
+	cout.width(7);
+	cout << a << endl;
+	cout.width(7);
+	cout << b << endl;
+
+	return 0;
