981e48c5a5da75300ed14ab4b7525bf496601a14
th3skil <94444564+th3skil@users.noreply.github.com>
2021-11-17 07:52:58 +0100

    Add files via upload

---
+++ zad 3 lista 2.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+void duze(char napis[]);
+int main()
+{
+
+	char tekst[1024];
+	cout << "podaj słowo: ";
+	cin >> tekst;
+	cout << "Napis przed modyfikacja: " << tekst << endl;
+	duze(tekst);
+	cout << "Napis po zmianie na duze litery: \n" << tekst << endl;
+	return 0;
+}
+void duze(char napis[])
+{
+	unsigned int kolejna_literka = 0;
+	char znak;
+	char roznica = 'A' - 'a';
+	while (znak = napis[kolejna_literka])
+	{
+		if ((znak >= 'a') && (znak <= 'z'))
+			napis[kolejna_literka]+= roznica;
+		kolejna_literka++;
+		
+	}
+}
